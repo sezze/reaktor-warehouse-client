ac50930f072a698deeb44303ab81e4ccfed28b8b
sezze <9763776+sezze@users.noreply.github.com>
2020-11-19 18:24:04 +0200

    Update header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import { FaGithub } from 'react-icons/fa';
+import sizes from '../constants/sizes';
 
 const HeaderContainer = styled.header`
-  background-color: ${({ theme }) => theme.surface};
-  box-shadow: ${({ theme }) => theme.shadow(2)};
   padding: 1rem;
   position: fixed;
   width: 100%;
@@ -13,16 +12,32 @@ const HeaderContainer = styled.header`
   z-index: 100;
 `;
 
+const GithubLink = styled.a`
+  display: block;
+  font-size: 2rem;
+  color: ${({ theme }) => theme.text};
+  width: 1em;
+  height: 1em;
+  padding: 4px;
+  background-color: ${({ theme }) => theme.surface};
+  border-radius: 50%;
+  box-shadow: ${({ theme }) => theme.shadow(8)};
+  transition: box-shadow 0.2s;
+
+  @media ${sizes.medium} {
+    box-shadow: ${({ theme }) => theme.shadow(0)};
+
+    &:hover {
+      box-shadow: ${({ theme }) => theme.shadow(8)};
+    }
+  }
+`;
+
 const Header: React.FC = () => (
   <HeaderContainer>
-    <span>Client</span>
-    <a href="">
+    <GithubLink href="https://github.com/sezze/reaktor-warehouse-client">
       <FaGithub />
-    </a>
-    <span>Server</span>
-    <a href="">
-      <FaGithub />
-    </a>
+    </GithubLink>
   </HeaderContainer>
 );
 
